f320cac7ace599b18987fd57e88dff904526a32a
meghapai <paimegha99@gmail.com>
2024-09-19 13:30:22 +0530

    exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = 0;
 
+  if (
+    start > 0 &&
+    end > 0 &&
+    Number.isInteger(start) &&
+    Number.isInteger(end) &&
+    typeof start === "number" &&
+    typeof end === "number"
+  ) {
+    if (start < end) {
+      for (let i = start; i <= end; i++) {
+        sum = sum + i;
+      }
+    } else if (start > end) {
+      for (let i = start; i >= end; i--) {
+        sum = sum + i;
+      }
+    }
+  } else {
+    return "ERROR";
+  }
+  return sum;
 };
 
 // Do not edit below this line
