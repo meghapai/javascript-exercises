db45cf422e53863d23eea61bf9e8490ab62a0f57
meghapai <paimegha99@gmail.com>
2024-09-19 12:46:48 +0530

    exercise 3.4 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
 
+let reversedString = reverseString("hello");
+console.log(reversedString);
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  // let index = array.indexOf(num);
+
+  // if (index > -1) {
+  //   array.splice(index, 1);
+  // }
+
+  // return array;
+  return array.filter((x) => !args.includes(x));
 };
 
+let newArray = removeFromArray([1, 2, 3, 4], 3);
+console.log(newArray);
 // Do not edit below this line
 module.exports = removeFromArray;
