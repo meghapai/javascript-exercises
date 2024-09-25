3aa663e84875a3d02e96339f60ee33b55188adfa
meghapai <paimegha99@gmail.com>
2024-09-25 23:05:40 +0530

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let currentYear = new Date().getFullYear();
 
+  return arr.reduce((oldest, person) => {
+    const deathYear = person.yearOfDeath || currentYear; // Use current year if yearOfDeath is undefined
+    const age = deathYear - person.yearOfBirth;
+
+    return age > (oldest.age || 0) ? { person, age } : oldest;
+  }, {}).person;
 };
 
 // Do not edit below this line
