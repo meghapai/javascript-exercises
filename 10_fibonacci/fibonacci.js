a1f60eafd4b2b6685bb65bcb949a2344b2f853d6
meghapai <paimegha99@gmail.com>
2024-09-25 22:10:10 +0530

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = Number(n);
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) return 0;
+  if (n === 1) return 1;
 
+  let a = 0; // F(0)
+  let b = 1; // F(1)
+
+  for (let i = 2; i <= n; i++) {
+    const temp = a + b; // F(n) = F(n-1) + F(n-2)
+    a = b; // Update F(n-1)
+    b = temp; // Update F(n)
+  }
+
+  return b; // F(n)
 };
 
 // Do not edit below this line
