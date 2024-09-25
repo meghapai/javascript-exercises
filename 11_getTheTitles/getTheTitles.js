0707d9003c9b7659ce82e8aba484b6ac0bee54c4
meghapai <paimegha99@gmail.com>
2024-09-25 22:16:05 +0530

    complete exercise 10

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  let bookArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    bookArr.push(arr[i].title);
+  }
+  return bookArr;
 };
 
 // Do not edit below this line
