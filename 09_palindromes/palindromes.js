73bd000c1deb0e49fe75e458c0008d079d202633
meghapai <paimegha99@gmail.com>
2024-09-25 20:58:00 +0530

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let validChar = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let palindrome = str
+    .trim()
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .split("")
+    .filter((str) => validChar.includes(str))
+    .join("");
 
+  let reverseStr = palindrome.split("").reverse().join("");
+
+  if (palindrome === reverseStr) return true;
+  else return false;
 };
+console.log(palindromes("racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
